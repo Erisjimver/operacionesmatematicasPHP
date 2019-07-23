165942c3e8932e400620254b51006791fed7f5cd
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-07-23 13:25:52 -0500

    estilos al resultado
    mejora en el codigo de la funcion y darle estilo a  los resultados

--- calculadora.php
+++ calculadora.php
@@ -1,3 +1,13 @@
+<style>
+  
+.resultado{
+  color: #F00;
+  font-weight: bold;
+  font-size: 32px;
+}
+
+</style>
+
 <?php
 
   if(isset($_POST["button"]))
@@ -17,27 +27,32 @@ function calcular($calculo){
     if(!strcmp("Suma",$calculo)){
       global $numero1;
       global $numero2;
-      echo "el resultado es: " . ($numero1+$numero2);
+      $resultado = $numero1+$numero2;
+      echo "<p class='resultado'> El resultado es: $resultado</p>";
     }
     if(!strcmp("Resta",$calculo)){
       global $numero1;
       global $numero2;   
-      echo "el resultado es: " . ($numero1-$numero2);
+      $resultado = $numero1-$numero2;
+      echo "<p class='resultado'> El resultado es: $resultado</p>";
     }
     if(!strcmp("Multiplicación",$calculo)){
       global $numero1;
       global $numero2;
-      echo "el resultado es: " . ($numero1*$numero2);
+      $resultado = $numero1*$numero2;
+      echo "<p class='resultado'> El resultado es: $resultado</p>";
     }
     if(!strcmp("División",$calculo)){
       global $numero1;
       global $numero2;
-      echo "el resultado es: " . ($numero1/$numero2);
+      $resultado = $numero1/$numero2;
+      echo "<p class='resultado'> El resultado es: $resultado</p>";
     }
     if(!strcmp("Módulo",$calculo)){
       global $numero1;
-      global $numero2;
-      echo "el resultado es: " . ($numero1%$numero2);
+      global $numero2;      
+      $resultado = $numero1%$numero2;
+      echo "<p class='resultado'> El resultado es: $resultado</p>";
     }
 
 }
